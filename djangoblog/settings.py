252fca8b213b53e0636a170c3c4e60d9c1be6fdf
zhao <838985328@qq.com>
2018-09-30 00:44:35 +0800

    database

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -81,25 +81,7 @@ DATABASES = {
     )
 }
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-#
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.mysql',
-#     #     'USER': 'root',
-#     #     'PASSWORD': 'root123',
-#     #     'NAME': 'miaoshao',
-#     #     'HOST': '69.12.64.148',
-#     #     'PORT': '3306',
-#     # }
-#
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators

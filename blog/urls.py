27e77e116f6c13344184b578b23e43763601ae1f
zhao <zhaoxinjie2016@gmail.com>
2018-12-25 03:28:32 +0800

    删除重复路径

--- blog/urls.py
+++ blog/urls.py
@@ -22,7 +22,6 @@ from blog.feeds import AllPostsRssFeed
 app_name = 'blog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^post/(?P<pk>[0-9]+)/', views.PostDetailView.as_view(), name='detail'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),

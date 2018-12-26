58c363d9c4c637729355003da4eb5fff141d269e
zhao <zhaoxinjie2016@gmail.com>
2018-12-26 23:58:02 +0800

    中间件

--- blog/middleware.py
+++ blog/middleware.py
@@ -25,14 +25,7 @@ class AccessMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        response = self.get_response(request)
-        if hasattr(self, 'process_request'):
-            response = self.process_request(request)
-        if not response:
-            response = self.get_response(request)
-        if hasattr(self, 'process_response'):
-            response = self.process_response(request, response)
-        return response
+        return self.get_response(request)
 
     def process_exception(self, request, exception):
         return HttpResponse(exception)
@@ -45,13 +38,6 @@ class AccessMiddleware(object):
                             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
                             meta['PATH_INFO'], meta['HTTP_REMOTE_ADDR'], meta['HTTP_USER_AGENT'],
                             meta['REQUEST_METHOD'], meta['QUERY_STRING']))
-        # logger.info('meta: ' + str(meta))
-        # 使用nginx代理添加了remote_addr
-        # if 'HTTP_X_FORWARDED_FOR' in request.META:  # 获取ip
-        #     client_ip = request.META['HTTP_X_FORWARDED_FOR']
-        #     client_ip = client_ip.split(",")[0]  # 所以这里是真实的ip
-        # else:
-        #     client_ip = request.META['REMOTE_ADDR']  # 这里获得代理ip
 
         if 'HTTP_REMOTE_ADDR' in request.META:
             client_ip = request.META['HTTP_REMOTE_ADDR']
